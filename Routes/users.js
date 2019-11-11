d836b52e53ec0d110aad5205ab23cf7d9eb175fe
Rodrigo Acevedo <rodrigoacevedo@outlook.com>
2019-11-10 23:56:24 -0300

    Base API

---
+++ Routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+
+    return res.send({
+        message: 'Tudo OK com o método GET da rota de usuários!'
+    });
+});
+
+router.post('/', (req,res) => {
+
+    return res.send({
+        message: 'Tudo OK com o método POST da rota de usuários!'
+    });
+});
+router.post('/create', (req,res) => {
+
+    return res.send({
+        message: 'Seu usuário foi criado =) !'
+    });
+});
+
+
+module.exports = router;
