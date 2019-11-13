9bd0213680d6f3d66739f9ef7b3a6c895fc5f229
Rodrigo Acevedo <rodrigoacevedo@outlook.com>
2019-11-12 23:27:07 -0300

    instalado jsonwebtoken e inserido status

--- Routes/index.js
+++ Routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth')
 
-router.get('/', (req,res) => {
+router.get('/', auth, (req,res) => {
 
+    console.log(res.locals.auth_data);
     return res.send({
-        message: 'Tudo OK com o método GET da raiz!'
+        message: 'Essa informação é muito importante. Usuários não autorizados não deveriam ver!'
     });
 });
 
